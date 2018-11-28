b3c0e65a13a44c6de64019e1b4c40f98020191ae
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-11-28 13:19:00 +0100

    added comment to explain the step_size addition

--- grasping_position_inference/inference/grid.py
+++ grasping_position_inference/inference/grid.py
@@ -6,6 +6,9 @@ class Grid(object):
         self._x_start, self._x_end, self._x_step_size = x_definition
         self._y_start, self._y_end, self._y_step_size = y_definition
 
+        #It is required to add the step size to create the real required grid size
+        #Numpy revokes the last cordinate during the grid creation
+
         self._x_end += self._x_step_size
         self._y_end += self._y_step_size
 
@@ -15,4 +18,4 @@ class Grid(object):
         x = np.arange(self._x_start, self._x_end, self._x_step_size)
         y = np.arange(self._y_start, self._y_end, self._y_step_size)
 
-        return np.meshgrid(x,y)
+        return np.meshgrid(x, y)
