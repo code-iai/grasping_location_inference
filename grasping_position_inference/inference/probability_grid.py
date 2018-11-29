f9ebcc8c80b989ca8d07e2955c92417d0f882220
Sebastian Koralewski <seba@cs.uni-bremen.de>
2018-11-29 13:39:34 +0100

    added step size calculating method

--- grasping_position_inference/inference/probability_grid.py
+++ grasping_position_inference/inference/probability_grid.py
@@ -1,16 +1,39 @@
 import numpy as np
 
+STEP_SIZE = 0.01
+MIN_X = -0.8
+MIN_Y = -0.8
+MAX_X = 0.8
+MAX_Y = 0.8
+
 
 class ProbabilityGrid(object):
     def __init__(self):
         self._grid = _init_grid()
 
     def __getitem__(self, key):
-        return self._grid[key[0], key[1]]
+        return self._grid[key[0]][key[1]]
 
 
 def _init_grid():
-    dimension = 1.6 / 0.01
+    dimension = 1.6 / STEP_SIZE
     dimension = int(round(dimension, 0)) + 1
 
     return np.full((dimension, dimension), 0.5)
+
+
+def _steps(start, end):
+    biggest = abs(end)
+    smallest = abs(start)
+
+    if smallest > biggest:
+        temp = smallest
+        smallest = biggest
+        biggest = temp
+
+    if np.sign(start) == np.sign(end):
+        distance = biggest - smallest
+    else:
+        distance = biggest + smallest
+
+    return int(round(distance / STEP_SIZE, 0))
--- grasping_position_inference/test/inference/probability_grid_test.py
+++ grasping_position_inference/test/inference/probability_grid_test.py
@@ -1,7 +1,17 @@
-from grasping_position_inference.inference.probability_grid import ProbabilityGrid
+from grasping_position_inference.inference.probability_grid import ProbabilityGrid, _steps
 
 
 def test_should_return_correct_probability_based_on_grid_cell():
     probability_grid = ProbabilityGrid()
 
     assert probability_grid[1, 2] == 0.5
+
+def test_should_return_correct_steps_numbers():
+    assert _steps(-0.8, -0.8) == 0
+    assert _steps(-0.8, 0.8) == 160
+    assert _steps(-0.8, -0.7) == 10
+    assert _steps(-0.7, -0.8) == 10
+    assert _steps(-0.7, 0) == 70
+    assert _steps(-0.7, 0.1) == 80
+    assert _steps(0.7, 0.1) == 60
+    assert _steps(0.7, -0.1) == 80
