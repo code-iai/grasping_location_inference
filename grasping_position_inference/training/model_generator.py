6a64a71b743c540f19b7b6cc7159a1addc0ce225
Sebastian Koralewski <seba@cs.uni-bremen.de>
2019-03-14 10:39:00 +0100

    feat(model_generator.py): Accepts only csv data as feature data

--- grasping_position_inference/training/model_generator.py
+++ grasping_position_inference/training/model_generator.py
@@ -9,10 +9,11 @@ DATA_PATH = path.join(ABSOLUTE_PATH, 'data')
 
 def generate_models(data_path=DATA_PATH):
     for data_filename in listdir(data_path):
-        model = Model(data_filename)
-        try:
-            model.train()
-            model.store()
-        except DataSetIsEmpty:
-            print 'Skipping {} since it is empty'.format(data_filename)
+        if data_filename.endswith('.csv'):
+            model = Model(data_filename)
+            try:
+                model.train()
+                model.store()
+            except DataSetIsEmpty:
+                print 'Skipping {} since it is empty'.format(data_filename)
 
